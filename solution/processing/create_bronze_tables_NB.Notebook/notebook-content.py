cb70dcaf4e4a09d4a48ade12cc1e8ca9564b4d7a
Abdulmaleek Mubaraq <mubaraqabdulmaleek@gmail.com>
2025-11-14 00:13:44 +0100

    wrote new  notebook to build empty tables in bronze layer lakehouse

---
+++ solution/processing/create_bronze_tables_NB.Notebook/notebook-content.py
@@ -0,0 +1,79 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+VL = notebookutils.variableLibrary.getLibrary("lakehouse_variables_VL")
+
+workspace_name = VL.FEATURE_WORKSPACE_NAME
+lakehouse_name = VL.BRONZE_LH_NAME
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+spark.sql(f"CREATE SCHEMA IF NOT EXISTS `{workspace_name}`.`{lakehouse_name}`.`youtube`")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+BRONZE_TABLE_SCHEMAS = {
+    "youtube.channel": """
+        channel_id STRING, 
+        channel_name STRING, 
+        channel_description STRING, 
+        view_count INT, 
+        subscriber_count INT, 
+        video_count INT, 
+        loading_TS TIMESTAMP""",
+    "youtube.playlist_items": """
+        channel_id STRING, 
+        video_id STRING, 
+        video_title STRING, 
+        video_description STRING,
+        thumbnail_url STRING,
+        video_publish_TS TIMESTAMP,
+        loading_TS TIMESTAMP""",
+    "youtube.videos": """
+        video_id STRING, 
+        video_view_count INT, 
+        video_like_count INT, 
+        video_comment_count INT,
+        loading_TS TIMESTAMP""", 
+}
+
+# for each key,value in the metadata object BRONZE_TABLE_SCHEMAS 
+for table, ddl in BRONZE_TABLE_SCHEMAS.items(): 
+    
+    # create a dynmamic Spark SQL script, reading from the Variable Library variables, and the BRONZE_TABLE_SCHEMAS metadata
+    # we use IF NOT EXISTS to avoid overwrite - all updates will be done through migrations
+    create_script = f"CREATE TABLE IF NOT EXISTS `{workspace_name}`.`{lakehouse_name}`.{table} ({ddl});" 
+    
+    # run the SQL statement to create the table
+    spark.sql(create_script)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
